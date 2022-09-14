a8eb9db965ab789838940c681c34d4c1f255908f
sondresorbye0111 <sondre@magson.no>
2022-09-14 11:20:59 +0200

    menu component

---
+++ src/header/header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./header.module.css";
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <h1>GitLab Stats Pro+</h1>
+        </header>
+    );
+}
---
+++ src/stats-page/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SideBar } from "./side-bar";
+
+export default function StatsPage() {
+    return (
+        <div className="flex">
+            <SideBar/>
+        </div>
+    );
+}
