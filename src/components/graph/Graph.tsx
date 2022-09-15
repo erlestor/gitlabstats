138ef2606ed943379e01fff5e9d0736b9ad9efe5
Erlend Storsve <erlestor@ntnu.no>
2022-09-15 19:36:23 +0200

    support for different timeframe and users

--- src/components/graph/Graph.tsx
+++ src/components/graph/Graph.tsx
@@ -9,6 +9,7 @@ import {
 } from "recharts"
 import "./graph.css"
 import { commits } from "./data"
+import { FC } from "react"
 
 interface Commit {
   authorName: string
@@ -16,52 +17,99 @@ interface Commit {
   numberOfCommits: number
 }
 
-const Graph = () => {
-  const dateSpan = ["2022-09-01", "2022-09-07"]
+interface GraphProps {
+  showUsers: string[]
+  timeFrame: string
+}
+
+const Graph: FC<GraphProps> = ({ showUsers, timeFrame }) => {
+  const getDateSpan = (): Date[] => {
+    const endDate = new Date()
+    if (timeFrame === "week") {
+      const startDate = new Date(
+        endDate.getFullYear(),
+        endDate.getMonth(),
+        endDate.getDate() - 7
+      )
+      return [startDate, endDate]
+    }
+    if (timeFrame === "month") {
+      const startDate = new Date(
+        endDate.getFullYear(),
+        endDate.getMonth() - 1,
+        endDate.getDate()
+      )
+      return [startDate, endDate]
+    }
+    if (timeFrame === "year") {
+      const startDate = new Date(
+        endDate.getFullYear() - 1,
+        endDate.getMonth(),
+        endDate.getDate()
+      )
+      return [startDate, endDate]
+    }
+    return []
+  }
+
+  const getDatesInRange = (startDate: any, endDate: any) => {
+    const date = new Date(startDate.getTime())
+    const dates = []
+    while (date <= endDate) {
+      dates.push(new Date(date))
+      date.setDate(date.getDate() + 1)
+    }
+    return dates
+  }
+
+  const getFormattedDate = (date: Date) => {
+    const day = date.getDate().toString()
+    const month = (date.getMonth() + 1).toString()
+    // const year = date.getFullYear().toString()
+    return day + "/" + month
+  }
+
+  const addDatesAsData = (data: any) => {
+    const dateSpan: Date[] = getDateSpan()
+    const datesInRange = getDatesInRange(dateSpan[0], dateSpan[1])
+    datesInRange.forEach((date) => {
+      const formattedDate = getFormattedDate(date)
+      let datapoint = { name: formattedDate }
+      showUsers.forEach((user) => {
+        datapoint = {
+          ...datapoint,
+          [user]: 0,
+        }
+      })
+      data.push(datapoint)
+    })
+  }
 
   const inDateSpan = (commit: Commit) => {
-    const commitDate = new Date(commit.date).getDate()
-    const startDate = new Date(dateSpan[0]).getDate()
-    const endDate = new Date(dateSpan[1]).getDate()
+    const dateSpan: Date[] = getDateSpan()
+    const commitDate = new Date(commit.date)
+    const startDate = dateSpan[0]
+    const endDate = dateSpan[1]
     return commitDate >= startDate && commitDate <= endDate
   }
 
   const getGraphData = () => {
     const data: any = []
+    addDatesAsData(data)
     const filteredCommits = commits.filter((c) => inDateSpan(c))
-    filteredCommits.forEach((commit: Commit) => {
+
+    filteredCommits.forEach((commit) => {
       const date = new Date(commit.date)
-      const day = date.getDate().toString()
-      const month = date.getMonth().toString()
-      const formattedDate = day + "/" + month
-
+      const formattedDate = getFormattedDate(date)
       const datapointIdx = data.findIndex((d: any) => d.name === formattedDate)
-
-      // hvis datoen allerede er lagt til -> legg til nøkkel til objekt
-      if (datapointIdx !== -1) {
-        const datapoint = data[datapointIdx]
-        data[datapointIdx] = {
-          ...datapoint,
-          [commit.authorName]: commit.numberOfCommits,
-        }
-      } else {
-        data.push({
-          name: formattedDate,
-          [commit.authorName]: commit.numberOfCommits,
-        })
+      data[datapointIdx] = {
+        ...data[datapointIdx],
+        [commit.authorName]: commit.numberOfCommits,
       }
     })
+
     return data
   }
-
-  const getUsers = () => {
-    const users = Array.from(
-      new Set(commits.map((commit: Commit) => commit.authorName))
-    )
-    return users
-  }
-
-  const users: any = getUsers()
 
   const strokeColors = ["#8884d8", "green", "red", "yellow", "blue"]
 
@@ -70,11 +118,20 @@ const Graph = () => {
       <h1>Commits</h1>
       <LineChart width={800} height={500} data={getGraphData()}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
+        <XAxis
+          dataKey="name"
+          // label={{ value: "date", position: "insideBottomRight", offset: 0 }}
+        />
+        <YAxis
+          label={{
+            value: "number of commits",
+            angle: -90,
+            position: "insideLeft",
+          }}
+        />
         <Tooltip />
-        <Legend />
-        {users.map((user: any, userIdx: number) => (
+        <Legend verticalAlign="bottom" />
+        {showUsers.map((user: any, userIdx: number) => (
           <Line
             key={userIdx}
             type="monotone"
