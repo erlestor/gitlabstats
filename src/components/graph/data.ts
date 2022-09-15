165da0a2747100eb91afe0d6926eeb2033fafb98
Erlend Storsve <erlestor@stud.ntnu.no>
2022-09-15 17:55:39 +0200

    test data

---
+++ src/components/graph/data.ts
@@ -0,0 +1,79 @@
+export const commits = [
+  // erlestor
+  {
+    authorName: "erlestor",
+    date: "2022-09-01",
+    numberOfCommits: 1,
+  },
+  {
+    authorName: "erlestor",
+    date: "2022-09-02",
+    numberOfCommits: 3,
+  },
+  {
+    authorName: "erlestor",
+    date: "2022-09-03",
+    numberOfCommits: 0,
+  },
+  {
+    authorName: "erlestor",
+    date: "2022-09-04",
+    numberOfCommits: 1,
+  },
+  {
+    authorName: "erlestor",
+    date: "2022-09-05",
+    numberOfCommits: 2,
+  },
+  {
+    authorName: "erlestor",
+    date: "2022-09-06",
+    numberOfCommits: 2,
+  },
+  {
+    authorName: "erlestor",
+    date: "2022-09-07",
+    numberOfCommits: 4,
+  },
+  // bruker1
+  {
+    authorName: "bruker1",
+    date: "2022-09-01",
+    numberOfCommits: 6,
+  },
+  {
+    authorName: "bruker1",
+    date: "2022-09-02",
+    numberOfCommits: 1,
+  },
+  {
+    authorName: "bruker1",
+    date: "2022-09-03",
+    numberOfCommits: 4,
+  },
+  {
+    authorName: "bruker1",
+    date: "2022-09-04",
+    numberOfCommits: 0,
+  },
+  {
+    authorName: "bruker1",
+    date: "2022-09-05",
+    numberOfCommits: 0,
+  },
+  {
+    authorName: "bruker1",
+    date: "2022-09-06",
+    numberOfCommits: 2,
+  },
+  {
+    authorName: "bruker1",
+    date: "2022-09-07",
+    numberOfCommits: 3,
+  },
+  {
+    authorName: "bruker1",
+    date: "2022-09-09",
+    numberOfCommits: 0,
+  },
+]
