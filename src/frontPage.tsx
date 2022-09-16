854d90c8498f3470f5e9cc9fd1bddcf3c70d8414
Magnus Løvhøiden <malovhoi@stud.ntnu.no>
2022-09-16 16:06:10 +0200

    Endret etter anbefalinger fra Sondre

--- src/frontPage.tsx
+++ src/frontPage.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "./frontPage.css";
+import styles from "./frontPage.module.css";
 import background from './images/background.jpg'
 
 export function FrontPage() {
     return (
-        <div style={{backgroundImage: `url(${background})`}} className="loginDiv">  
-            <div className="formDiv">
+        <div style={{backgroundImage: `url(${background})`}} className={styles.loginDiv}>  
+            <div className={styles.formDiv}>
                 <form>
                     <h3>LOGG INN</h3>
                     <input placeholder="Brukernavn" type="text"/>
