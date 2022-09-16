2a0d99c3f282173d96daea35296789a91c0626de
Magnus Løvhøiden <malovhoi@stud.ntnu.no>
2022-09-16 20:05:35 +0200

    Loginform service

--- src/frontPage.tsx
+++ src/frontPage.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import styles from "./frontPage.module.css";
 import background from './images/background.jpg'
+import LoginForm  from './LoginForm'
 
 export function FrontPage() {
     return (
         <div style={{backgroundImage: `url(${background})`}} className={styles.loginDiv}>  
-            <div className={styles.formDiv}>
-                <form>
-                    <h3>LOGG INN</h3>
-                    <input placeholder="Brukernavn" type="text"/>
-                    <input placeholder="Passord" type="password"/>
-                    <button>Logg inn</button>
-                </form>
-            </div>
+            <LoginForm/>
         </div>
     );
 }
