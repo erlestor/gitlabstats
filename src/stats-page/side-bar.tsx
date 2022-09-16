b12a2e2e7f086b5f6d0b55c8b833ddea99cfdbb9
sondresorbye0111 <sondre@magson.no>
2022-09-16 14:25:47 +0200

    #5 fixed menu component state and added main component with scroll wrapper

--- src/stats-page/side-bar.tsx
+++ src/stats-page/side-bar.tsx
@@ -1,22 +1,39 @@
-import React from "react";
+import { ChangeEvent, useState } from "react";
 import styles from "./side-bar.module.css";
 
 export function SideBar() {
-  const persons = ["Magnus", "Erik", "Erlend", "Sondre"];
+  const personsArray = ["Magnus", "Erik", "Erlend", "Sondre"];
+  const p: { [key: string]: boolean } = {};
+  for (const person of personsArray) {
+    p[person] = true;
+  }
   const timeFrames = ["Last week", "Last month", "Last year"];
+
+  const [persons, setPersons] = useState(p);
+  const [selectedTimeFrame, setSelectedTimeFrame] = useState(timeFrames[0]);
+
+  function selectPersonChange(evt: ChangeEvent<HTMLInputElement>) {
+    setPersons({ ...persons, [evt.target.value]: evt.target.checked });
+  } 
+
   return (
     <div className={styles.sideBar}>
       <h3>Persons</h3>
-      {persons.map((person) => (
+      {personsArray.map((person) => (
         <div key={person}>
-          <input type="checkbox" value={person} />
+          <input checked={persons[person]} onChange={selectPersonChange} type="checkbox" value={person} />
           <label>{person}</label>
         </div>
       ))}
       <h3>Time frame</h3>
       {timeFrames.map((timeFrame) => (
         <div key={timeFrame}>
-          <input type="radio" value={timeFrame} />
+          <input
+            checked={timeFrame == selectedTimeFrame}
+            onChange={() => setSelectedTimeFrame(timeFrame)}
+            type="radio"
+            value={timeFrame}
+          />
           <label>{timeFrame}</label>
         </div>
       ))}
