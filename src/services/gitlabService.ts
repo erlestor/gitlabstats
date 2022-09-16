6bc97a731016e237f060e38a281ca0de0090ee2c
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2022-09-16 11:18:29 +0200

    #2 Trying to get commits done by a user

--- src/services/gitlabService.ts
+++ src/services/gitlabService.ts
@@ -5,7 +5,10 @@ const token = "glpat-gG3CkJFYeo4nVrLmcDRa"; // gitlab access token. Add your own
 const baselineUrl = "https://gitlab.stud.idi.ntnu.no/api/v4/projects/";
 
 //First get all members function
-export const getAllMembers = async (id: number): Promise<any[]> => {
+export const getAllMembers = async (id: number) => {
+
+  var dataList:string[] = [] 
+
   return await axios
     .get(
       baselineUrl + `${id}/members/all`,
@@ -36,6 +39,23 @@ export const getCommits = async (projectId: number): Promise<any[]> => {
         console.log(response.data);
       if (response.status === 200) {
         return response.data;
+      }
+      return null;
+    });
+}
+
+//This does not work and I dont understand why
+export const getAllCommitsOfUser = async (projectId: number, userId: number): Promise<any[]> => {
+  return await axios
+    .get(baselineUrl + `${projectId}/repository/commits`, {
+      headers: {
+          Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+      //filter the commits by the user id
+      if (response.status === 200) {
+        return response.data.filter((commit: any) => commit.author_id === userId);
       }
       return null;
     });
--- src/test/gitlabService.test.ts
+++ src/test/gitlabService.test.ts
@@ -1,4 +1,4 @@
-import { getAllMembers, getCommits } from "../services/gitlabService";
+import { getAllCommitsOfUser, getAllMembers, getCommits } from "../services/gitlabService";
 
 describe("Gitlab service", () => {
   it("should be able to get all users in repo", async () => {
@@ -11,5 +11,12 @@ describe("Gitlab service", () => {
     const id = 17450;
     const result = await getCommits(id);
     expect(result.length).toBeGreaterThan(0);
-});
+  });
+  it("should be able to get all commits belonging to a specific user", async () => {
+    const id = 17450;
+    //sondreso user id should get results
+    const userId = 3586;
+    const result = await getAllCommitsOfUser(id, userId);
+    expect(result.length).toBeGreaterThan(0);
+  });
 });
