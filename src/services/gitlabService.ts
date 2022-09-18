1b7030ed93efd9de325c36a38f71a09c2bc5a4d8
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2022-09-18 18:24:41 +0200

    #2 #7 Filter commits by start and end dates

--- src/services/gitlabService.ts
+++ src/services/gitlabService.ts
@@ -29,8 +29,7 @@ export const getAllMembers = async (id: number) => {
 
 //Get commits from the gitlab repository
 export const getCommits = async (projectId: number): Promise<any[]> => {
-  return await axios
-    .get(baselineUrl + `${projectId}/repository/commits`, {
+  return await axios.get(baselineUrl + `${projectId}/repository/commits`, {
       headers: {
           Authorization: `Bearer ${token}`,
       },
@@ -44,10 +43,27 @@ export const getCommits = async (projectId: number): Promise<any[]> => {
     });
 }
 
+//Get commits sorted on given dates
+export const getCommitsByDates = async (projectId: number, startDate: string, endDate: string): Promise<any[]> => {
+  return await axios.get(baselineUrl + `${projectId}/repository/commits?since=${startDate}&until=${endDate}`, {
+      headers: {
+          Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+        console.log(response.data);
+      if (response.status === 200) {
+        console.log(response.status);
+        return response.data;
+      }
+      return null;
+    });
+
+}
+
 //This does not work and I dont understand why
 export const getAllCommitsOfUser = async (projectId: number, userId: number): Promise<any[]> => {
-  return await axios
-    .get(baselineUrl + `${projectId}/repository/commits`, {
+  return await axios.get(baselineUrl + `${projectId}/repository/commits`, {
       headers: {
           Authorization: `Bearer ${token}`,
       },
