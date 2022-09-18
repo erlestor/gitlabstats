de27feb057c3d070f910e7a63015c9eb5453e745
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2022-09-18 19:44:04 +0200

    #11 Rewrite tests to work with single commit func

--- src/test/gitlabService.test.ts
+++ src/test/gitlabService.test.ts
@@ -1,24 +1,40 @@
-import { getAllCommitsOfUser, getAllMembers, getCommits, getCommitsByDates } from "../services/gitlabService";
+import { getAllMembers, getCommits } from "../services/gitlabService";
+
+const startDate = "2022-09-14T09:13:31.000+02:00";
+const endDate = "2022-09-14T13:49:38.000+02:00";
+const id = 17450;
 
 describe("Gitlab service", () => {
   it("should be able to get all users in repo", async () => {
-    const id = 17450;
+    
     const result = await getAllMembers(id);
     expect(result.length).toBeGreaterThan(0);
   });
 
   it("should be able to get all commits in repo", async () => {
-    const id = 17450;
+    
     const result = await getCommits(id);
     expect(result.length).toBeGreaterThan(0);
   });
 
   it("should be able to get commits between two dates", async () => {
-    const id = 17450;
+    
     //Two different times on the inital day of project setup
-    const startDate = "2022-09-14T09:13:31.000+02:00";
-    const endDate = "2022-09-14T13:49:38.000+02:00";
-    const result = await getCommitsByDates(id, startDate, endDate);
+    const result = await getCommits(id, startDate, endDate);
+    expect(result.length).toBeGreaterThan(0);
+  });
+
+  it("should be able to get commits since a date", async () => {
+    
+    //Two different times on the inital day of project setup
+    const result = await getCommits(id, startDate);
+    expect(result.length).toBeGreaterThan(0);
+  });
+
+  it("should be able to get commits until a date", async () => {
+    
+    //Two different times on the inital day of project setup
+    const result = await getCommits(id, null, endDate);
     expect(result.length).toBeGreaterThan(0);
   });
 });
