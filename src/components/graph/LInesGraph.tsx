14f2a9f1614e2b288de8666b5485061de9c27fe4
Erlend Storsve <erlestor@ntnu.no>
2022-09-20 15:36:59 +0200

    #1 use bar chart in LinesGraph

--- src/components/graph/LInesGraph.tsx
+++ src/components/graph/LInesGraph.tsx
@@ -1,31 +1,40 @@
 import {
-    PieChart,
-    Pie,
-  } from "recharts"
-  import "./graph.css"
-  import { FC } from "react"
-  import { getGraphData } from "../../service/commitsToGraph"
-  
-  interface GraphProps {
-    showUsers: string[]
-    timeFrame: string
-  }
-  
-  const  LinesGraph: FC<GraphProps> = ({ showUsers, timeFrame }) => {
-    const colors = ["#8884d8", "green", "red", "yellow", "blue"]
-    const graphData = getGraphData(timeFrame, showUsers)
+  PieChart,
+  Pie,
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Bar,
+} from "recharts"
+import "./graph.css"
+import { FC } from "react"
+import { linesWritten } from "../../service/linesWritten"
 
-    const test1 = [{name: "erlestor", value: 100}, {name: "bruker1", value: 70}]
-  
-    return (
-      <div className="chart-container">
-        <h1>Commits</h1>
-        <PieChart width={730} height={450}>
-            <Pie data={test1} dataKey="value" nameKey="name" outerRadius={150} fill="#8884d8"  />
-        </PieChart>
-      </div>
-    )
-  }
-  
-  export default LinesGraph
-  
+interface GraphProps {
+  showUsers: string[]
+  timeFrame: string
+}
+
+const LinesGraph: FC<GraphProps> = ({ showUsers, timeFrame }) => {
+  const colors = ["#8884d8", "green", "red", "yellow", "blue"]
+
+  const data = linesWritten
+
+  return (
+    <div className="chart-container">
+      <h1>Commits</h1>
+      <BarChart width={730} height={250} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey="lines" fill="#8884d8" />
+      </BarChart>
+    </div>
+  )
+}
+
+export default LinesGraph
