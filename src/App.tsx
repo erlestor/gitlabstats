8c0e05e1d76a5ad4c0e6156bcedda9278900cacf
Magnus Løvhøiden <malovhoi@stud.ntnu.no>
2022-09-23 13:00:13 +0200

    #16 Add functionality to login button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { FrontPage } from './frontPage';
+import StatsPage from './stats-page';
 
 function App() {
+
+  // TODO: set to save value from localStorage
+  const [showFrontPage, setShowFrontPage] = useState(true);
+
+  if (!showFrontPage) {
+    return <StatsPage/>
+  }
+
   return (
-    <FrontPage/>
+    <FrontPage callback={(token, password) => {
+      setShowFrontPage(false);
+    }}/>
   );
 }
 
