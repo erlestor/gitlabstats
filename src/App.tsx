5bf57d345ce40f022b7df80ea42e4b345299f999
Erlend Storsve <erlestor@ntnu.no>
2022-09-27 19:41:57 +0200

    #1 remove unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import "./App.css"
 import CommitsGraph from "./components/graph/CommitsGraph"
-import LinesGraph from "./components/graph/LInesGraph"
 
 function App() {
   const showUsers = ["erlestor", "bruker1"]
-  const timeFrame = "week"
+  const timeFrame = "month"
 
   return (
     <div className="App">
       <CommitsGraph showUsers={showUsers} timeFrame={timeFrame} />
-      <LinesGraph showUsers={showUsers} timeFrame={timeFrame} />
     </div>
   )
 }
