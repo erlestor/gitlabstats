e94d8e6db6cdc30c8294e2ba730a64ac79deb4e1
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2022-09-29 11:04:23 +0200

    #20 Snapshot test of App

--- src/test/snapshot.test.tsx
+++ src/test/snapshot.test.tsx
@@ -2,9 +2,13 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 describe ('App, component hierarcy', () => {
-    it('renders correctly', () => {
-        const tree = render
-        
-    }
+
+    //Snapshot test of the DOM structure. 
+    it('renders DOM correctly', () => {
+        const tree = render(<App />);
+        expect(tree).toMatchSnapshot();
+    });
 });
+
+
 
