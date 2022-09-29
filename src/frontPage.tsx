0f9d4efe176c01539a4bb14409c43921611c8cdf
sondresorbye0111 <sondre@magson.no>
2022-09-29 10:21:43 +0200

    #23 added scalable image

--- src/frontPage.tsx
+++ src/frontPage.tsx
@@ -1,12 +1,19 @@
-import React from "react";
 import styles from "./frontPage.module.css";
-import background from './images/background.jpg'
-import LoginForm, { LoginFormProps }  from './LoginForm'
+import backgroundBig from "./images/backgroundBig.jpg";
+import backgroundSmall from "./images/backgroundSmall.jpg";
+import LoginForm, { LoginFormProps } from "./LoginForm";
 
 export function FrontPage(props: LoginFormProps) {
-    return (
-        <div style={{backgroundImage: `url(${background})`}} className={styles.loginDiv}>  
-            <LoginForm callback={props.callback}/>
-        </div>
-    );
-}  
+  return (
+    <div
+      style={{
+        backgroundImage: `-webkit-image-set(
+            url(${backgroundSmall}) 2x,
+            url(${backgroundBig}) 3x)`,
+      }}
+      className={styles.loginDiv}
+    >
+      <LoginForm callback={props.callback} />
+    </div>
+  );
+}
