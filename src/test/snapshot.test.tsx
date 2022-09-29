0941475bdfb4eff1fcdb417b8262e6127d4d43f5
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2022-09-29 11:10:40 +0200

    #20 add commenting of snapshot

--- src/test/snapshot.test.tsx
+++ src/test/snapshot.test.tsx
@@ -3,12 +3,14 @@ import App from '../App';
 
 describe ('App, component hierarcy', () => {
 
-    //Snapshot test of the DOM structure. 
+    //Snapshot test of the DOM structure at start up.
     it('renders DOM correctly', () => {
         const tree = render(<App />);
         expect(tree).toMatchSnapshot();
     });
 });
+
+
 
 
 
