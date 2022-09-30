b9f5a0c29333da804898cc96b622820ce8fd083d
Erlend Storsve <erlestor@stud.ntnu.no>
2022-09-30 16:00:05 +0200

    #1 show label only when graph is visible

--- src/components/graph/CommitsGraph.tsx
+++ src/components/graph/CommitsGraph.tsx
@@ -41,49 +41,51 @@ const CommitsGraph: FC<GraphProps> = ({ showUsers, timeFrame }) => {
   return (
     <div className="chart-container">
       <h1>Commits</h1>
-      <h5>number of commits</h5>
       {showUsers.length > 0 ? (
-        <ResponsiveContainer width="100%" height={450}>
-          <AreaChart data={graphData} margin={{ left: -35 }}>
-            <defs>
-              {colors.map((color, colorIdx) => (
-                <linearGradient
-                  key={colorIdx}
-                  id={color}
-                  x1="0"
-                  y1="0"
-                  x2="0"
-                  y2="1"
-                >
-                  <stop offset="5%" stopColor={color} stopOpacity={0.8} />
-                  <stop offset="95%" stopColor={color} stopOpacity={0} />
-                </linearGradient>
-              ))}
-            </defs>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-              dataKey="name"
-              // label={{ value: "date", position: "insideBottomRight", offset: 0 }}
-            />
-            <YAxis />
-            <Tooltip />
-            <Legend verticalAlign="bottom" />
-            {showUsers.map((user, userIdx) => {
-              const color = colors[userIdx]
-              const fill = "url(#" + color + ")"
-              return (
-                <Area
-                  key={userIdx}
-                  type="monotone"
-                  dataKey={user}
-                  stroke={color}
-                  fillOpacity={0.8}
-                  fill={fill}
-                />
-              )
-            })}
-          </AreaChart>
-        </ResponsiveContainer>
+        <>
+          <h5>number of commits</h5>
+          <ResponsiveContainer width="100%" height={450}>
+            <AreaChart data={graphData} margin={{ left: -35 }}>
+              <defs>
+                {colors.map((color, colorIdx) => (
+                  <linearGradient
+                    key={colorIdx}
+                    id={color}
+                    x1="0"
+                    y1="0"
+                    x2="0"
+                    y2="1"
+                  >
+                    <stop offset="5%" stopColor={color} stopOpacity={0.8} />
+                    <stop offset="95%" stopColor={color} stopOpacity={0} />
+                  </linearGradient>
+                ))}
+              </defs>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                dataKey="name"
+                // label={{ value: "date", position: "insideBottomRight", offset: 0 }}
+              />
+              <YAxis />
+              <Tooltip />
+              <Legend verticalAlign="bottom" />
+              {showUsers.map((user, userIdx) => {
+                const color = colors[userIdx]
+                const fill = "url(#" + color + ")"
+                return (
+                  <Area
+                    key={userIdx}
+                    type="monotone"
+                    dataKey={user}
+                    stroke={color}
+                    fillOpacity={0.8}
+                    fill={fill}
+                  />
+                )
+              })}
+            </AreaChart>
+          </ResponsiveContainer>
+        </>
       ) : (
         <h3>Select one or more users to view graph</h3>
       )}
