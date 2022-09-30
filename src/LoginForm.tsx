2cece4651e6188fe7f53eaf8605d3c9705f0a97b
Erlend Storsve <erlestor@stud.ntnu.no>
2022-09-30 17:31:48 +0200

    #19 validate projectid and token
    Co-authored-by: Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>

--- src/LoginForm.tsx
+++ src/LoginForm.tsx
@@ -1,37 +1,54 @@
-import React from "react";
-import styles from "./frontPage.module.css";
-import { RepoInformation } from "./getRepoInformation";
+import React from "react"
+import styles from "./frontPage.module.css"
+import { getRepoInformation, RepoInformation } from "./getRepoInformation"
+import { checkIfValidId, getAllMembers } from "./services/gitlabService"
 
 export type LoginFormProps = {
-  callback: (repoInformation: RepoInformation) => void;
-};
+  callback: (repoInformation: RepoInformation) => void
+}
 
 export default class LoginForm extends React.Component<
   LoginFormProps,
   {
-    token: string;
-    projectId: number | undefined;
+    token: string
+    projectId: number | undefined
+    error: string
   }
 > {
   constructor(props: LoginFormProps) {
-    super(props);
+    super(props)
 
     this.state = {
       token: "",
       projectId: undefined,
-    };
+      error: "",
+    }
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ ...this.state, [event.target.name]: event.target.value });
+    this.setState({ ...this.state, [event.target.name]: event.target.value })
   }
 
-  handleSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
-    event.preventDefault();
-    this.props.callback(this.state as RepoInformation);
+  async handleSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const token = this.state.token
+    const projectId = this.state.projectId
+
+    await checkIfValidId(Number(projectId), token)
+      .then((valid) => {
+        if (valid) {
+          this.setState({ error: "" })
+          this.props.callback(this.state as RepoInformation)
+          return
+        }
+        this.setState({ error: "Wrong token or projectId" })
+      })
+      .catch((err) => {
+        this.setState({ error: "Wrong token or projectId" })
+      })
   }
 
   render() {
@@ -57,8 +74,11 @@ export default class LoginForm extends React.Component<
             required
           />
           <button data-testid="loginBtn">Logg inn</button>
+          {this.state.error.length > 0 && (
+            <p className={styles.error}>{this.state.error}</p>
+          )}
         </form>
       </div>
-    );
+    )
   }
 }
