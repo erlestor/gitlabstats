dc853957c223cb40ba3e6e2bd044b3302b050884
Erlend Storsve <erlestor@stud.ntnu.no>
2022-09-30 14:16:23 +0200

    #1 store filterOptions in sessionStorage

--- src/getRepoInformation.ts
+++ src/getRepoInformation.ts
@@ -1,44 +1,59 @@
-import {} from "./services/gitlabService";
+import {} from "./services/gitlabService"
+import { FilterOptions } from "./stats-page/IFilterOptions"
 
 export interface RepoInformation {
-  token: string;
-  projectId: number;
+  token: string
+  projectId: number
 }
 
-let repoInformation: RepoInformation | null = null;
+let repoInformation: RepoInformation | null = null
 
 const getLocalStorageToken = (): string | null => {
-  return localStorage.getItem("token");
-};
+  return localStorage.getItem("token")
+}
 
 const getLocalStorageProjectId = (): number | null => {
-  const v = localStorage.getItem("projectId");
+  const v = localStorage.getItem("projectId")
   if (!v) {
-    return null;
+    return null
   }
-  return parseInt(v);
-};
+  return parseInt(v)
+}
 
 export const hasRepoInformation = (): boolean => {
-  return !!repoInformation || (!!getLocalStorageToken() && !!getLocalStorageProjectId());
+  return (
+    !!repoInformation ||
+    (!!getLocalStorageToken() && !!getLocalStorageProjectId())
+  )
 }
 
 export const getRepoInformation = (): RepoInformation => {
   if (repoInformation) {
-    return repoInformation;
+    return repoInformation
   }
   if (getLocalStorageToken() && getLocalStorageProjectId()) {
     repoInformation = {
       token: getLocalStorageToken()!,
       projectId: getLocalStorageProjectId()!,
-    };
-    return repoInformation;
+    }
+    return repoInformation
   }
-  throw new Error("No repo information found");
-};
+  throw new Error("No repo information found")
+}
 
 export const saveRepoInformation = (r: RepoInformation) => {
-  repoInformation = r;
-  localStorage.setItem("token", r.token);
-  localStorage.setItem("projectId", r.projectId.toString());
-};
+  repoInformation = r
+  localStorage.setItem("token", r.token)
+  localStorage.setItem("projectId", r.projectId.toString())
+}
+
+export const saveFilterInformation = (filterOptions: FilterOptions) => {
+  window.sessionStorage.setItem("filterOptions", JSON.stringify(filterOptions))
+}
+
+export const getFilterInformation = () => {
+  const sessionStorage = window.sessionStorage.getItem("filterOptions")
+
+  if (sessionStorage) return JSON.parse(sessionStorage)
+  return null
+}
