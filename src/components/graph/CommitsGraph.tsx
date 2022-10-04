d18110852e3a3e0dba6733f3832b2ae6eb9607ac
Erlend Storsve <erlestor@stud.ntnu.no>
2022-10-04 16:31:39 +0200

    #29 revert commitsgraph

--- src/components/graph/CommitsGraph.tsx
+++ src/components/graph/CommitsGraph.tsx
@@ -49,45 +49,55 @@ const CommitsGraph: FC<GraphProps> = ({ selectedUsers, timeFrame }) => {
 
   return (
     <div className="chart-container">
-      <h5>number of commits</h5>
-      <ResponsiveContainer width="100%" height={450}>
-        <AreaChart data={graphData} margin={{ left: -35 }}>
-          <defs>
-            {colors.map((color, colorIdx) => (
-              <linearGradient
-                key={colorIdx}
-                id={color}
-                x1="0"
-                y1="0"
-                x2="0"
-                y2="1"
-              >
-                <stop offset="5%" stopColor={color} stopOpacity={0.8} />
-                <stop offset="95%" stopColor={color} stopOpacity={0} />
-              </linearGradient>
-            ))}
-          </defs>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend verticalAlign="bottom" />
-          {Array.from(selectedUsers).map((user, userIdx) => {
-            const color = colors[userIdx]
-            const fill = "url(#" + color + ")"
-            return (
-              <Area
-                key={userIdx}
-                type="monotone"
-                dataKey={user}
-                stroke={color}
-                fillOpacity={0.8}
-                fill={fill}
+      <h1>Commits</h1>
+      {selectedUsers.size > 0 ? (
+        <>
+          <h5>number of commits</h5>
+          <ResponsiveContainer width="100%" height={450}>
+            <AreaChart data={graphData} margin={{ left: -35 }}>
+              <defs>
+                {colors.map((color, colorIdx) => (
+                  <linearGradient
+                    key={colorIdx}
+                    id={color}
+                    x1="0"
+                    y1="0"
+                    x2="0"
+                    y2="1"
+                  >
+                    <stop offset="5%" stopColor={color} stopOpacity={0.8} />
+                    <stop offset="95%" stopColor={color} stopOpacity={0} />
+                  </linearGradient>
+                ))}
+              </defs>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                dataKey="name"
+                // label={{ value: "date", position: "insideBottomRight", offset: 0 }}
               />
-            )
-          })}
-        </AreaChart>
-      </ResponsiveContainer>
+              <YAxis />
+              <Tooltip />
+              <Legend verticalAlign="bottom" />
+              {Array.from(selectedUsers).map((user, userIdx) => {
+                const color = colors[userIdx]
+                const fill = "url(#" + color + ")"
+                return (
+                  <Area
+                    key={userIdx}
+                    type="monotone"
+                    dataKey={user}
+                    stroke={color}
+                    fillOpacity={0.8}
+                    fill={fill}
+                  />
+                )
+              })}
+            </AreaChart>
+          </ResponsiveContainer>
+        </>
+      ) : (
+        <h4>Select one or more users to view graph</h4>
+      )}
     </div>
   )
 }
