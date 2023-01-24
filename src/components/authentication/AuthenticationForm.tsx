c3db1442ff22a9ea4ba12202913256a15ab690f2
Erlend Storsve <erlestor@ntnu.no>
2023-01-24 17:04:18 +0100

    added title/icon

--- src/components/authentication/AuthenticationForm.tsx
+++ src/components/authentication/AuthenticationForm.tsx
@@ -1,31 +1,31 @@
-import React from "react";
-import { RepoInformation } from "../../services/getRepoInformation";
-import { validateRepoInformation } from "../../services/gitlabService";
-import styles from "./authenticationPage.module.css";
+import React from "react"
+import { RepoInformation } from "../../services/getRepoInformation"
+import { validateRepoInformation } from "../../services/gitlabService"
+import styles from "./authenticationPage.module.css"
 
 export type AuthenticationFormProps = {
-  callback: (repoInformation: RepoInformation) => void;
-};
+  callback: (repoInformation: RepoInformation) => void
+}
 
 export default class AuthenticationForm extends React.Component<
   AuthenticationFormProps,
   {
-    token: string;
-    projectId: number | string;
-    error: string;
+    token: string
+    projectId: number | string
+    error: string
   }
 > {
   constructor(props: AuthenticationFormProps) {
-    super(props);
+    super(props)
 
     this.state = {
       token: "",
       projectId: "",
       error: "",
-    };
+    }
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   /**
@@ -34,22 +34,22 @@ export default class AuthenticationForm extends React.Component<
    * Uses event to access the targeted input field and get its new value
    */
   handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ ...this.state, [event.target.name]: event.target.value });
+    this.setState({ ...this.state, [event.target.name]: event.target.value })
   }
 
   async handleSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const token = this.state.token;
-    const projectId = Number(this.state.projectId);
+    event.preventDefault()
+    const token = this.state.token
+    const projectId = Number(this.state.projectId)
 
     await validateRepoInformation({ projectId, token })
       .then(() => {
-        this.setState({ error: "" });
-        this.props.callback(this.state as RepoInformation);
+        this.setState({ error: "" })
+        this.props.callback(this.state as RepoInformation)
       })
       .catch((_) => {
-        this.setState({ error: "Could not verify repository information" });
-      });
+        this.setState({ error: "Could not verify repository information" })
+      })
   }
 
   render() {
@@ -58,7 +58,7 @@ export default class AuthenticationForm extends React.Component<
         <form onSubmit={this.handleSubmit}>
           <h3>Insert Repository Information</h3>
           <input
-            placeholder="Token"
+            placeholder="Gitlab Token"
             type="text"
             name="token"
             value={this.state.token}
@@ -80,6 +80,6 @@ export default class AuthenticationForm extends React.Component<
           )}
         </form>
       </div>
-    );
+    )
   }
 }
